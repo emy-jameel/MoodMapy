00926d48cfa5006ccdaf3422114d74cd2ea45bdb
emy-jameel <emyjameel2@gmail.com>
2026-02-13 22:36:17 +0300

    Update project and prepare for GitHub migration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.mood_map"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
